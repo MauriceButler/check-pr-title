8d7df46613bea04c1bc6ae464f71b8eec76dd7f8
Maurice Butler <maurice.butler@gmail.com>
2021-10-22 19:03:37 +1000

    use require

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-import { context } from '@actions/github';
-import { getInput, setFailed } from '@actions/core';
+const github = require('@actions/github');
+const core = require('@actions/core');
 
 async function run() {
-    const regexString = getInput('regex');
+    const regexString = core.getInput('regex');
     const regex = new RegExp(regexString);
-    const title = context.payload?.pull_request?.title;
+    const title = github.context.payload?.pull_request?.title;
 
     if (!regex.test(title)) {
-        setFailed(`Pull request title "${title}" does not match regex "${regexString}"`);
+        core.setFailed(`Pull request title "${title}" does not match regex "${regexString}"`);
     }
 }
 
